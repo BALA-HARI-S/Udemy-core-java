132b930c3ebd269c9ef60bc6883f85da13a77874
BALA-HARI-S <balahari754@gmail.com>
2023-11-08 13:04:56 +0530

    "Section 8"

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -8,6 +8,22 @@ public class Main {
         bossBaby.watchMovie();
         bossBaby.watchComedy();
 
+        System.out.println("Coding Exercise : 38 : Composition");
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12,55);
+
+        Bed bed = new Bed("Modern", 4,3,2,1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("My Room", wall1, wall2, wall3,
+                wall4, ceiling, bed, lamp);
+        bedRoom.makeBed();
+        bedRoom.getLamp().turnOn();
 
     }
 }
